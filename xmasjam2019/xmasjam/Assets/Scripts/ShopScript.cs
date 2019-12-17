4af4707a2a8e2b1d655fdc131554250b1739cf62
Bram <bram.wolthuis@hotmail.com>
2019-12-17 12:55:38 +0100

    Money counter
    geeft weer hoeveel geld de speler heeft

--- xmasjam2019/xmasjam/Assets/Scripts/ShopScript.cs
+++ xmasjam2019/xmasjam/Assets/Scripts/ShopScript.cs
@@ -8,18 +8,36 @@ using UnityEngine.UI;
 public class ShopScript : MonoBehaviour
 {
     public Text shopButtonText;
+    public Text moneyText;
     public GameObject shopWindow;
     bool isWindowOpen;
+    int money;
 
     void Start()
     {
         isWindowOpen = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateMoney();
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (money < 9999)
+            money = money + 5;
+
+            if (money > 9999)
+                money = 9999;
+        }
+
+    }
+
+    void updateMoney()
+    {
+        moneyText.text = "Money: " + money.ToString("0000");
     }
 
     public void ShopButton()

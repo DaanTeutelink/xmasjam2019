50d047cd6fad8162fc93c3dfe10726c735bb256f
Bram <bram.wolthuis@hotmail.com>
2019-12-17 12:00:59 +0100

    Shop UI
    it can open and close

---
+++ xmasjam2019/xmasjam/Assets/Scripts/ShopScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ShopScript : MonoBehaviour
+{
+    public Text shopButtonText;
+    public GameObject shopWindow;
+    bool isWindowOpen;
+
+    void Start()
+    {
+        isWindowOpen = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShopButton()
+    {
+        isWindowOpen = !isWindowOpen;
+        shopWindow.SetActive(isWindowOpen);
+        if (isWindowOpen)
+        {
+            shopButtonText.text = "Close";
+        }
+        else if (!isWindowOpen)
+        {
+            shopButtonText.text = "Shop";
+        }
+
+
+    }
+
+}
